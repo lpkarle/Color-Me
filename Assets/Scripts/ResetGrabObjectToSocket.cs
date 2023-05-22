74a53dc70e8a655625e3a25d7edcc27378909a1c
Lukas Karle <28999846+lpkarle@users.noreply.github.com>
2023-05-22 10:23:29 +0200

    Fix picker and palette to socket script.

--- Assets/Scripts/ResetGrabObjectToSocket.cs
+++ Assets/Scripts/ResetGrabObjectToSocket.cs
@@ -9,30 +9,19 @@ public class ResetGrabObjectToSocket : MonoBehaviour
     [SerializeField]
     Transform SocketAttachpoint;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {   
-        
-    }
-
     public async void resetToAttachpoint()
     {
         await Delay(3000);
-        this.transform.position = SocketAttachpoint.position;
-        this.transform.rotation = SocketAttachpoint.rotation;
 
+        if (this != null)
+        {
+            this.transform.position = SocketAttachpoint.position;
+            this.transform.rotation = SocketAttachpoint.rotation;
+        }
     }
 
     private async Task Delay(int milliseconds)
     {
-        Debug.Log("START DELAY");
-
         await Task.Delay(milliseconds);
     }
 }
